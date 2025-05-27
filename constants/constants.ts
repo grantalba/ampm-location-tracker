293df19b124f7354319499cbe95a4b6bd6338289
Grant <grant@Grants-MacBook-Pro.local>
2025-05-27 14:34:38 +0800

    feat: remove large pngs

--- constants/constants.ts
+++ constants/constants.ts
@@ -8,19 +8,19 @@ export type OnboardingScreensType = {
 export const onboarding_screens: OnboardingScreensType[] = [
   {
     id: 1,
-    image: require("@/assets/images/splash_1.png"),
+    image: require("@/assets/images/favicon.png"),
     title: "Track Your Location",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus varius nulla, quis dictum augue.",
   },
   {
     id: 2,
-    image: require("@/assets/images/splash_2.png"),
+    image: require("@/assets/images/favicon.png"),
     title: "Search Location",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus varius nulla, quis dictum augue.",
   },
   {
     id: 3,
-    image: require("@/assets/images/splash_3.png"),
+    image: require("@/assets/images/favicon.png"),
     title: "Light/Dark Theme",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus varius nulla, quis dictum augue.",
   },
